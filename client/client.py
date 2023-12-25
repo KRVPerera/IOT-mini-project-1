fcde7e8d1863412a1d73bd525f74bbfae3936783
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2023-12-25 10:09:47 +0200

    asd

--- client/client.py
+++ client/client.py
@@ -15,7 +15,7 @@ async def main():
         random_float = random.uniform(22.0, 45.0)
         randString = str(random_float)
         payload = (randString).encode("utf-8")
-        request = Message(code=aiocoap.POST, payload=payload , uri='coap://127.0.0.1:5683/temp')
+        request = Message(code=aiocoap.POST, payload=payload , uri='coap://[2600:1f16:15a8:3b2:804b:8136:56a6:cb5b]:5683/temp')
         try:
             response = await protocol.request(request).response
         except Exception as e:
