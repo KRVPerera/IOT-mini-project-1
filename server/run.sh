1130f685e079037cbcbfb56db277821994834d60
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-31 10:57:06 +0100

    Add filters for sensor tags in dashboard.json and update run.sh script

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+sudo docker stop server-grafana-1
+sudo docker stop server-coap_server-1
+sudo docker container prune
+sudo docker system prune -a
 cd app
 sudo docker build . --no-cache
 cd ..
