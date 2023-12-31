4d1a94a28ed53b2ce448603c42f4de8e7d38bb7c
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-31 15:13:47 +0100

    Update start scripts for different locations

--- start_lille.sh
+++ start_lille.sh
@@ -15,13 +15,7 @@ for (( i=0; i<3; i++ )); do
     mynodes+=( "$number" )
 done
 
-
-echo "Array Contents: ${mynodes[*]}"
-
 iotlab-node --flash gnrc_border_router.elf -l "lille,m3,${mynodes[1]}" -i "${ID}"
 iotlab-node --flash firmware/sensor_lille.elf -l "lille,m3,${mynodes[2]}" -i "${ID}"
-
-echo "nc m3-2 20000"
 sudo ethos_uhcpd.py m3-${mynodes[0]} tap9 2001:660:4403:0489::1/64
-echo "Script ended."
 iotlab-experiment stop -i ${ID}
